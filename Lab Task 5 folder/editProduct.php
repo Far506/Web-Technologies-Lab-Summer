b7ce4e2ac48a79e8d1612b78b9cc4c2c173dd0eb
Far506 <98295031+Far506@users.noreply.github.com>
2022-07-22 05:31:38 -0700

    Add files via upload

---
+++ Lab Task 5 folder/editProduct.php
@@ -0,0 +1,33 @@
+<?php 
+
+require_once 'controller/productInfo.php';
+$product = fetchProduct($_GET['id']);
+
+
+ include "nav.php";
+
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+ <form action="controller/updateProduct.php" method="POST" enctype="multipart/form-data">
+  <label for="name">Name:</label><br>
+  <input value="<?php echo $product['Name'] ?>" type="text" id="name" name="name"><br>
+  <label for="surname">Surname:</label><br>
+  <input value="<?php echo $product['Surname'] ?>" type="text" id="surname" name="surname"><br>
+  <label for="username">User Name:</label><br>
+  <input value="<?php echo $product['Username'] ?>" type="text" id="username" name="username"><br>
+  <input type="file" name="image"><br><br>
+  <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">
+  <input type="submit" name = "updateProduct" value="Update">
+  <input type="reset"> 
+</form> 
+
+</body>
+</html>
---
+++ Lab Task 5 folder/nav.php
@@ -0,0 +1,5 @@
+<ul>
+        <li><a href="addProduct.php"> Add product</a></li>
+        <li><a href="showAllProducts.php"> Show all products</a></li>
+        <li><a href="searchUser.php"> Search Products</a></li>
+    </ul>
