b52f0263c325b5c07850aff73195745df62bdd4b
Far506 <98295031+Far506@users.noreply.github.com>
2022-07-29 22:08:29 +0600

    Add files via upload

---
+++ Lab task 6 folder/mysql databse table.php
@@ -0,0 +1,8 @@
+CREATE TABLE `lab 6` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4
