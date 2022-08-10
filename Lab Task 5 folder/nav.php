4bba3aabbf4a25d6926e6b155ef343f591f5b98d
Far506 <98295031+Far506@users.noreply.github.com>
2022-08-10 09:40:05 -0700

    Delete nav.php

--- Lab Task 5 folder/nav.php
+++
@@ -1,5 +0,0 @@
-<ul>
-        <li><a href="addProduct.php"> Add product</a></li>
-        <li><a href="showAllProducts.php"> Show all products</a></li>
-        <li><a href="searchUser.php"> Search Products</a></li>
-    </ul>
